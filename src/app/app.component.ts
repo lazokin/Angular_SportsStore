cf38380160ea40c86594be1349be4059fac20f28
Nik Ambukovski <lazokin@hotmail.com>
2018-12-11 14:45:03 +1100

    preparing the angular project features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'SportsStore';
-}
+  selector: "app",
+  template: `<div class="bg-success p-2 text-center text-white">
+              This is SportsStore
+            </div>`
+  })
+  export class AppComponent {}
